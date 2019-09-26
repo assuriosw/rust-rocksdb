808632b366d3448c47bf798fe5c86c8887b1410f
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-09-26 10:59:19 +0300

    Update rocksdb submodule to 6.1.2 (#328)

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -110,8 +110,8 @@ fn build_rocksdb() {
     config.define("NDEBUG", Some("1"));
 
     let mut lib_sources = include_str!("rocksdb_lib_sources.txt")
-        .trim()
-        .split(" ")
+        .split("\n")
+        .map(str::trim)
         .collect::<Vec<&'static str>>();
 
     // We have a pregenerated a version of build_version.cc in the local directory
