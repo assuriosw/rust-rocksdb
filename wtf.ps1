ae0e27ffb3092434cca28da9e848dbc01fcb40d4
Adam Nelson <anelson@users.noreply.github.com>
2019-10-29 17:53:28 +0100

    No easy day...

--- wtf.ps1
+++ wtf.ps1
@@ -1,6 +1,6 @@
 # Un-escape the escaped colons caused by a CI system that claims to be "enterprise" but has no concept of Windows paths
-$env:RUSTUP_HOME=($env:RUSTUP_HOME -replace "|DRONE_COLON|",":")
-$env:CARGO_HOME=($env:CARGO_HOME -replace "|DRONE_COLON|",":")
+$env:RUSTUP_HOME=$env:RUSTUP_HOME | %{ $_.Replace("|DRONE_COLON|", ":") }
+$env:CARGO_HOME=$env:CARGO_HOME | %{ $_.Replace("|DRONE_COLON|", ":") }
 get-command 'cargo.exe' | select Source
 C:/ProgramData/scoop/apps/rustup-msvc/current/.cargo/bin/cargo.exe version
 cargo version
