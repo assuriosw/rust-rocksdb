9fb7450e92e62644230677e193700926567bdbac
Adam Nelson <anelson@users.noreply.github.com>
2019-10-29 16:57:35 +0100

    Holy messed up environment variables, is this the problem??

--- wtf.ps1
+++ wtf.ps1
@@ -1,3 +1,5 @@
+$env:RUSTUP_HOME = "C:\\ProgramData\\scoop\\persist\\rustup-msvc\\.rustup"
+$env:CARGO_HOME = "C:\\ProgramData\\scoop\\persist\\rustup-msvc\\.cargo"
 dir C:/ProgramData/scoop/apps/rustup-msvc/current/.cargo/bin
 get-command 'cargo.exe' | select Source
 C:/ProgramData/scoop/apps/rustup-msvc/current/.cargo/bin/cargo.exe version
