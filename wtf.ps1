f7107ccb2c9e3d3ad799c4786468e8f1011c242d
Adam Nelson <anelson@users.noreply.github.com>
2019-10-29 17:37:58 +0100

    This could be a viable if ugly workaround

--- wtf.ps1
+++ wtf.ps1
@@ -1,5 +1,6 @@
-$env:RUSTUP_HOME="$env:SystemDrive\\$env:RUSTUP_HOME"
-$env:CARGO_HOME="$env:SystemDrive\\$env:CARGO_HOME"
+# Un-escape the escaped colons caused by a CI system that claims to be "enterprise" but has no concept of Windows paths
+$env:RUSTUP_HOME=$env:RUSTUP_HOME -replace "|DRONE_COLON|",":"
+$env:CARGO_HOME=$env:CARGO_HOME -replace "|DRONE_COLON|",":"
 get-command 'cargo.exe' | select Source
 C:/ProgramData/scoop/apps/rustup-msvc/current/.cargo/bin/cargo.exe version
 cargo version
