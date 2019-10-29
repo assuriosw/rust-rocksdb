33aa02c14228f85bd3bc0832f7bce75d2c8cd39e
Adam Nelson <anelson@users.noreply.github.com>
2019-10-29 17:31:26 +0100

    Try shitty workaround

--- wtf.ps1
+++ wtf.ps1
@@ -1,4 +1,5 @@
-dir env:
+$env:RUSTUP_HOME="$env:SystemDrive\\$env:RUSTUP_HOME"
+$env:CARGO_HOME="$env:SystemDrive\\$env:CARGO_HOME"
 get-command 'cargo.exe' | select Source
 C:/ProgramData/scoop/apps/rustup-msvc/current/.cargo/bin/cargo.exe version
 cargo version
