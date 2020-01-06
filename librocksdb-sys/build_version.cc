30ea8e96949064efb773de812e7f4cc7073bc6e3
Adam Nelson <anelson@users.noreply.github.com>
2020-01-06 13:55:38 +0100

    Update RocksDB to `v6.5.2` release

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:d47cdbc1888440a75ecf43646fd1ddab8ebae9be";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-10-30";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:4cfbd87afd08a16df28436fb990ef6b154ee6114";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-01-06";
 const char* rocksdb_build_compile_date = __DATE__;
