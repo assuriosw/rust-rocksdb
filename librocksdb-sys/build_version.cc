6384cff966bbdf28153a5c3cd42a0b19205a1bdd
anelson <anelson@users.noreply.github.com>
2020-02-10 15:01:38 +0200

    Update to RocksDB v6.6.4 (#2)
    This isn't just a routine update.  This enables `format_version=5`, a
    new on-disk format that uses a vastly improved Bloom filter for much
    better query performance.
    
    It also uses a new 64-bit optimized hash algorithm for non-persistent
    hash use cases, which uses AVX2 instructions (meaning it requires a
    Haswell or later processor, circa 2014).
    
    This is a pre-req for https://github.com/assuriosw/assurio/issues/37

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:4cfbd87afd08a16df28436fb990ef6b154ee6114";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-01-06";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:551a110918493a19d11243f53408b97485de1411";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-02-10";
 const char* rocksdb_build_compile_date = __DATE__;
