48c7b7fd2368bc41aea511b60dbc68295ab889e6
Nik Bond <nikbond97@gmail.com>
2020-05-05 13:48:25 +0300

    Update bindgen and uuid in dependencies of librocksdb-sys
    Summary:
    Crate librocksdb-sys and crates in assurio repository use different versions of
    bindgen and uuid. We should update them to the latest versions for avoiding
    duplicates of dependencies.
    
    Changes:
     - Update bindgen to 0.53
     - Update uuid to 0.8
     - Update version of rocksdb submodule in librocksdb-sys and change the appropriative files,
       such as rocksdb_lib_sources.txt and build_version.cc.
     - Consider size_t in new version of FFI as usize in Rust code.
     - Increment librocksdb-sys version to 6.6.5

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:551a110918493a19d11243f53408b97485de1411";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-02-10";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:211088df6e997d2d73ceba4395f570df5f856737";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-05-05";
 const char* rocksdb_build_compile_date = __DATE__;
